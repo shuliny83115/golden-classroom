00f784700610c7dc90c8d8f9a758bf70b3d1a107
shuliny83115 <shuliny83115@gmail.com>
2026-08-23 14:47:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -490,7 +490,7 @@ function ensureVmVideo() {
   rtcVideo.style.height = "100%";
   rtcVideo.style.objectFit = "contain";
   rtcVideo.style.background = "#111827";
-  rtcVideo.style.cursor = "none";
+  rtcVideo.style.cursor = "default";
   rtcVideo.tabIndex = 0;
 
   vmScreen.appendChild(rtcVideo);
