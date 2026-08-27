d8793c215aba7a74efbdf066b099866682888f03
shuliny83115 <shuliny83115@gmail.com>
2026-08-27 10:01:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1525,21 +1525,21 @@ rtcControlChannel.onerror = (err) => {
 });
   const codecs = RTCRtpReceiver.getCapabilities("video").codecs;
 
-const vp8 = codecs.filter(
-  c => c.mimeType.toLowerCase() === "video/vp8"
+const vp9 = codecs.filter(
+  c => c.mimeType.toLowerCase() === "video/vp9"
 );
 
 const others = codecs.filter(
-  c => c.mimeType.toLowerCase() !== "video/vp8"
+  c => c.mimeType.toLowerCase() !== "video/vp9"
 );
 
-if (vp8.length > 0) {
+if (vp9.length > 0) {
   videoTransceiver.setCodecPreferences([
-    ...vp8,
+    ...vp9,
     ...others
   ]);
 
-  console.log("VIEWER VP8 PREFERRED");
+  console.log("VIEWER VP9 PREFERRED");
 }
 
   rtcPeer.ontrack = (event) => {
